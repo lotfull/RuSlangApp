4b164b3416c9a5ba3056a016ee047c719bef748c
Kam Lotfull <kamlotfull@gmail.com>
2017-03-11 18:14:19 +0300

    firstCommmit, adding all files

---
+++ SlangApp/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  SlangApp
+//
+//  Created by Kam Lotfull on 24.01.17.
+//  Copyright © 2017 Kam Lotfull. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ SlangApp/viewController.swift
@@ -0,0 +1,23 @@
+//
+//  viewController.swift
+//  SlangApp
+//
+//  Created by Kam Lotfull on 11.03.17.
+//  Copyright © 2017 Kam Lotfull. All rights reserved.
+//
+
+import UIKit
+
+class viewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+    
+    
+    
+    
+    
+}
