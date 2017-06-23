3fde90ffe1c1d10d26f4f9930e8047d4a091cc94
Kam Lotfull <kamlotfull@gmail.com>
2017-06-23 15:03:33 +0300

    First big try to parse twitter in Python, modified and save some first parse data. Now it's no more actual, but interesting to look.

--- SlangApp/Twitter parsing py.py
+++ SlangApp/Twitter parsing py.py
@@ -28,21 +28,20 @@ public_tweets = api.home_timeline()
 
 count = 0
 last_count = 0
-checkout = 200
-step = 200
-limit = 600
+step = 100
+limit = 1000
 
-words_names = [u"дратути", u"путлер", u"тян", u"шавуха", u"ето ", u"зашквар", u"бро", u"брутальный", u"зига", u"лузер", u"марамойка", u"омг", u"ололо", u"онотоле", u"педовка", u"стафф", u"фейк", u"хасл", u"хейтер", u"азаз", u"сорян", u"млять", u"госпади", u"лол", u"кек", u"навальный", u"ипаный", u"нахой", u"ахуенно", u"жиза", u"усманов", u"димон", u"путен", u"блэт", u"нэвэльный", u"россея", u"фак", u"рашка", u"сук "]
-mat_words = [u"бля", u"сука", u"хуй", u"ебать", u"пиздец", u"ахуенно"]
-shhh_words = [u"щас", u"шо ", u"ща ", u"чо "]
+words_names = [u"дратути", u"путлер", u"тян", u"шавуха", u"ето ", u"зашквар", u"бро", u"брутальный", u"зига", u"лузер", u"марамойка", u"омг", u"ололо", u"онотоле", u"педовка", u"стафф", u"фейк", u"хасл", u"хейтер", u"азаз", u"сорян", u"млять", u"госпади", u"лол", u"кек", u"навальный", u"ипаный", u"нахой", u"ахуенно", u"жиза", u"усманов", u"димон", u"путен", u"блэт", u"нэвэльный", u"россея", u"фак", u"рашка", u"сук ", u"патимейкер", u"трешер", u"дратуте", u"зашквар", u"флексить", u"рофл", u"вейпер", u"хайп", u"эба", u"сап двач", u"оптимус гэнг", u"ауе", u"писос", u"краш", u"по фану", u"шерь", u"шерить", u"15 год", u"байт", u"шарю", u"шарить", u"ауф", u"шкура", u"тамблер герл", u"стафф", u"амахасла", u"трип", u"крип", u"ору", u"лакшери", u"гомункул", u"лампово", u"чсв", u"свитшот", u"тп", u"Бэкграунд", u"Потрачено", u"кк", u"шавуха", u"челендж", u"панч", u"жи есть", u"хаер", u"кун", u"баляховка"]
+mat_words = [u"бля", u"сука", u"хуй", u"ебать", u"пиздец", u"ахуенно", u"ебана", u"пиздато"]
+shhh_words = [u"щас", u"шо ", u"ща ", u"чо ", u"изи"]
 #words_names.extend(mat_words).extend(shhh_words)
 words_count = {}
 every_words_count = {}
 
-def print_dict_sorted_by_value(name, dictionary):
+def print_dict_sorted_by_value(name, dictionary, top):
     print("\n\n*********\n", name, "\n*********\n\n")
     for (key, value) in sorted(dictionary.items(), key=lambda x: x[1], reverse=True):
-        if value < 5:
+        if value < top:
             break
         print(key, ": ", value, sep='')
 
@@ -57,7 +56,8 @@ def every_word_count_in(sentence):
 
 class listener(StreamListener):
     def on_data(self, data):
-        global count, last_count, words_names, words_count, checkout, step, limit
+        global count, last_count, words_names, words_count, step, limit
+        checkout = step
         data = json.loads(data)
         last_count = count
         #matchers = ['abc','def'] # words_names
@@ -65,9 +65,9 @@ class listener(StreamListener):
         every_word_count_in(text)
         if count > checkout:
             checkout += step
-            print_dict_sorted_by_value("SLANG WORDS", words_count)
+            print_dict_sorted_by_value("SLANG WORDS", words_count, 1)
             if checkout > limit:
-                print_dict_sorted_by_value("ALL WORDS", every_words_count)
+                print_dict_sorted_by_value("ALL WORDS", every_words_count, 5)
                 return False
         for word_name in words_names:
             if word_name in text:
@@ -75,21 +75,20 @@ class listener(StreamListener):
                 print(count, word_name)
                 count += 1
         if last_count == count:
-            print("********LOL, no coincidence********")
-
+            print("********", "".join(text.split('\n')))
         return True
 
     def on_error(self, status):
         print(status)
 
 starttime=time.time()
-timeout = 1000.0
+timeout = 10000.0
 
 while True:
     print("tick")
     twitterStream = Stream(auth, listener())
     twitterStream.filter(track=words_names, async=True)
     time.sleep(timeout)
-    print_dict_sorted_by_value("SLANG WORDS", words_count)
-    print_dict_sorted_by_value("ALL WORDS", every_words_count)
+    print_dict_sorted_by_value("SLANG WORDS", words_count, 1)
+    print_dict_sorted_by_value("ALL WORDS", every_words_count, 5)
     break
